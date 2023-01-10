15272ef563deccfaaa35871d8ba735afb63731da
Tyson <tyson@G1-HCM-L-M-0041.local>
2023-01-10 23:56:32 +0700

    Update script

--- arw.sh
+++ arw.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-subfinder -d $1 -o subdomains.txt | httprobe -c 50 -t 1000 | wfuzz -w wordlist.txt -c -u 'https://$1' -H 'X-Forwarded-For: FUZZ' -v --hc 404 | grep -e "code-200" | awk '{print $5) | grep -E.php.asp.jsp | hakcheckurl -verbose grep | -E 'high|medium' | sort -u > vuln_url.txt
+subfinder -d $1 -o subdomains.txt | httprobe -c 50 -t 1000 | wfuzz -w wordlist.txt -c -u 'https://$1' -H 'X-Forwarded-For: FUZZ' -v --hc 404 | grep -e "code-200" | awk '{print $5) | grep -E '.php.asp.jsp' | hakcheckurl -verbose 
+| grep -E 'high|medium' | sort -u > vuln_url.txt
